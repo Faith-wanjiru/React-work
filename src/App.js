56a9169b8de275cddf37af6d261da192bf4c8ac9
Faith-wanjiru <wanjirufaithwanyoike@gmail.com>
2024-06-20 16:15:25 +0300

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
+
+// import About from "./About"
+import React from 'react';
+import Login from "./Login";
+import Users from "./Users";
+
+
+// export function Introduction  (){
+//      return(
+//       <h1>We are Adalab</h1>
+//      )
+// }
+
+
+
+const App = () =>{
+return(
+    <div>
+      {<Login/>}
+      <Users/>
     </div>
-  );
+  )
 }
-
 export default App;
---
+++ src/Users/index.js
@@ -0,0 +1,28 @@
+import "./index.css";
+import React from 'react'
+import { useGetUsers } from "./hooks/useGetUsers";
+import UserCard from "../Atoms/UserCards"; 
+const Users= () =>{
+    const{users, error, loading} = useGetUsers();
+    console.log ({users});
+    return(
+        <div className='images'>
+        {loading && <h2>Loading users...</h2>}
+        {error.length>0 && <h2>{error}</h2>}
+        {users.length>0? users.map((user) =>(
+            <UserCard
+            key={user.id}
+            image={user.image}
+            fullName={`${user.firstName} ${user.lastName} ${user.age} ${user.gender} `}
+           />
+        )): !loading&& <h2>No users found</h2>
+             
+
+        }
+          
+           
+        </div>
+    )
+}
+
+export default Users;
